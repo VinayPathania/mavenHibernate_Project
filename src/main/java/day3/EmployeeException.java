70724ba4e8d4e6528a5a775b08a9d22d779470c1
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-10-14 07:38:30 +0530

    implementation methods updated

---
+++ src/main/java/day3/EmployeeException.java
@@ -0,0 +1,8 @@
+package day3;
+
+public class EmployeeException extends Exception{
+    public EmployeeException(String message) {
+        System.out.println(message);
+    }
+
+}
--- src/main/java/day3/empDaoImpl.java
+++ src/main/java/day3/empDaoImpl.java
@@ -8,8 +8,6 @@ public class empDaoImpl implements empDao {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("MavenUnit");
     EntityManager em = emf.createEntityManager();
 
-    EmployeeException ee = new EmployeeException();
-
 
     @Override
     public void save(emp Emp) throws EmployeeException{
@@ -21,7 +19,7 @@ public class empDaoImpl implements empDao {
             System.out.println("Record inserted");
             em.close();
         }catch (Exception e){
-            ee.getMessage();
+            throw new EmployeeException("record not added");
         }
 
     }
@@ -30,31 +28,35 @@ public class empDaoImpl implements empDao {
     public String getEmployee(int empId) throws EmployeeException{
         emp Emp = em.find(emp.class,empId);
         if(Emp!= null) return Emp.toString();
-        else return ee.getMessage();
+        else  throw new EmployeeException("Record not found");
     }
 
     @Override
     public String deleteEmployee(int empId) throws EmployeeException{
-        String message = "Record not found";
-        try{
+        String message;
             emp Emp = em.find(emp.class,empId);
-            if (Emp==null) return message;
+            if (Emp==null) throw new EmployeeException("Record not found");
             else{
                 em.getTransaction().begin();
                 em.remove(empId);
                 em.getTransaction().commit();
                 em.close();
-                return message = ("Record deleted");
+                 message = ("Record deleted");
             }
-
-        }catch (Exception e){
-            message = ee.getMessage();
-        }
         return message;
     }
 
     @Override
     public boolean updateEmployee(emp Emp) throws EmployeeException{
+        boolean flag = false;
+         Emp = em.find(emp.class,Emp.getEmpId());
+         if(Emp==null) throw new EmployeeException("Invalid account");
+         else{
+            em.getTransaction().begin();
+            em.merge(Emp);
+            flag  = true;
+            em.getTransaction().commit();
+         }
         return false;
     }
 
